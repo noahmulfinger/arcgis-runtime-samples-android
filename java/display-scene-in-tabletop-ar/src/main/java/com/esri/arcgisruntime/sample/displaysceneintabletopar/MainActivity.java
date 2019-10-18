5665de565df75ae6a85f1a6659b72fafc4798e54
trev8939 <tdraeseke@esri.com>
2019-10-18 15:51:06 +0100

    integrated mesh layer as tabletop

--- java/display-scene-in-tabletop-ar/src/main/java/com/esri/arcgisruntime/sample/displaysceneintabletopar/MainActivity.java
+++ java/display-scene-in-tabletop-ar/src/main/java/com/esri/arcgisruntime/sample/displaysceneintabletopar/MainActivity.java
@@ -16,12 +16,9 @@
 
 package com.esri.arcgisruntime.sample.displaysceneintabletopar;
 
-import java.util.List;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -31,21 +28,15 @@ import android.view.MotionEvent;
 import android.widget.Toast;
 
 import com.esri.arcgisruntime.concurrent.ListenableFuture;
-import com.esri.arcgisruntime.geometry.Envelope;
-import com.esri.arcgisruntime.geometry.GeodeticCurveType;
-import com.esri.arcgisruntime.geometry.GeometryEngine;
-import com.esri.arcgisruntime.geometry.LinearUnit;
-import com.esri.arcgisruntime.geometry.LinearUnitId;
 import com.esri.arcgisruntime.geometry.Point;
-import com.esri.arcgisruntime.loadable.LoadStatus;
+import com.esri.arcgisruntime.layers.IntegratedMeshLayer;
 import com.esri.arcgisruntime.mapping.ArcGISScene;
-import com.esri.arcgisruntime.mapping.MobileScenePackage;
+import com.esri.arcgisruntime.mapping.ArcGISTiledElevationSource;
 import com.esri.arcgisruntime.mapping.NavigationConstraint;
+import com.esri.arcgisruntime.mapping.Surface;
 import com.esri.arcgisruntime.mapping.view.Camera;
 import com.esri.arcgisruntime.mapping.view.DefaultSceneViewOnTouchListener;
 import com.esri.arcgisruntime.toolkit.ar.ArcGISArView;
-import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -72,117 +63,60 @@ public class MainActivity extends AppCompatActivity {
     mArView = findViewById(R.id.arView);
     mArView.registerLifecycle(getLifecycle());
 
-    // show simple instructions to the user. Refer to the README for more details
-    Toast.makeText(this,
-        "Move the camera back and forth over a plane. When a plane is detected, tap on the plane to place a scene",
-        Toast.LENGTH_LONG).show();
+    ArcGISTiledElevationSource elevationSource = new ArcGISTiledElevationSource(
+        "https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer");
+    Surface surface = new Surface();
+    surface.getElevationSources().add(elevationSource);
+
+    IntegratedMeshLayer integratedMeshLayer = new IntegratedMeshLayer(
+        "https://tiles.arcgis.com/tiles/FQD0rKU8X5sAQfh8/arcgis/rest/services/VRICON_Yosemite_Sample_Integrated_Mesh_scene_layer/SceneServer");
+
+
+
+    ArcGISScene scene = new ArcGISScene();
+    scene.getOperationalLayers().add(integratedMeshLayer);
+
+    scene.setBaseSurface(surface);
 
     // on tap
     mArView.getSceneView().setOnTouchListener(new DefaultSceneViewOnTouchListener(mArView.getSceneView()) {
       @Override public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
-        // get the hit results for the tap
-        List<HitResult> hitResults = mArView.getArSceneView().getArFrame().hitTest(motionEvent);
-        // check if the tapped point is recognized as a plane by ArCore
-        if (!hitResults.isEmpty() && hitResults.get(0).getTrackable() instanceof Plane) {
-          // get a reference to the tapped plane
-          Plane plane = (Plane) hitResults.get(0).getTrackable();
-          Toast.makeText(MainActivity.this, "Plane detected with a width of: " + plane.getExtentX(), Toast.LENGTH_SHORT)
-              .show();
-          // get the tapped point as a graphics point
-          android.graphics.Point screenPoint = new android.graphics.Point(Math.round(motionEvent.getX()),
-              Math.round(motionEvent.getY()));
-          // if initial transformation set correctly
-          if (mArView.setInitialTransformationMatrix(screenPoint)) {
-            // the scene hasn't been configured
-            if (!mHasConfiguredScene) {
-              loadSceneFromPackage(plane);
-            } else if (mArView.getSceneView().getScene() != null) {
-              // use information from the scene to determine the origin camera and translation factor
-              updateTranslationFactorAndOriginCamera(mArView.getSceneView().getScene(), plane);
-            }
-          }
+        // get the tapped point as a graphics point
+        android.graphics.Point screenPoint = new android.graphics.Point(Math.round(motionEvent.getX()),
+            Math.round(motionEvent.getY()));
+        // if initial transformation set correctly
+        if ( mArView.setInitialTransformationMatrix(screenPoint)) {
+          mArView.getSceneView().setScene(scene);
+          // when the scene has loaded, set navigation constraint and opacity to see below the surface
+          scene.addDoneLoadingListener(() -> {
+            // ensure the navigation constraint is set to NONE
+            scene.getBaseSurface().setNavigationConstraint(NavigationConstraint.NONE);
+            // set opacity to view content beneath the base surface
+            scene.getBaseSurface().setOpacity(0.5f);
+            // set translation factor and origin camera for scene placement in AR
+            // set the translation factor based on scene content width and desired physical size
+            mArView.setTranslationFactor(500);
+            // find the center point of the scene content
+            Point centerPoint = integratedMeshLayer.getFullExtent().getCenter();
+            // find the altitude of the surface at the center
+            ListenableFuture<Double> elevationFuture = mArView.getSceneView().getScene().getBaseSurface()
+                .getElevationAsync(centerPoint);
+            elevationFuture.addDoneListener(() -> {
+              try {
+                double elevation = elevationFuture.get();
+                // create a new origin camera looking at the bottom center of the scene
+                mArView.setOriginCamera(
+                    new Camera(new Point(centerPoint.getX(), centerPoint.getY(), elevation), 1000, 0, 90, 0));
+              } catch (Exception e) {
+                Log.e(TAG, "Error getting elevation at point: " + e.getMessage());
+              }
+            });
+          });
         } else {
-          String error = "ArCore doesn't recognize this point as a plane.";
-          Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
-          Log.e(TAG, error);
+          Toast.makeText(MainActivity.this, "ARCore did not detect the tapped point as a plane", Toast.LENGTH_LONG).show();
         }
         return super.onSingleTapConfirmed(motionEvent);
       }
-    });
-  }
-
-  /**
-   * Load the mobile scene package and get the first (and only) scene inside it. Set it to the ArView's SceneView and
-   * set the base surface to opaque and remove any navigation constraint, thus allowing the user to look at a scene
-   * from below. Then call updateTranslationFactorAndOriginCamera with the plane detected by ArCore.
-   *
-   * @param plane detected by ArCore based on a tap from the user. The loaded scene will be pinned on this plane.
-   */
-  private void loadSceneFromPackage(Plane plane) {
-    // create a mobile scene package from a path a local .mspk
-    MobileScenePackage mobileScenePackage = new MobileScenePackage(
-        Environment.getExternalStorageDirectory() + getString(
-            R.string.philadelphia_mobile_scene_package_path));
-    // load the mobile scene package
-    mobileScenePackage.loadAsync();
-    mobileScenePackage.addDoneLoadingListener(() -> {
-      // if it loaded successfully and the mobile scene package contains a scene
-      if (mobileScenePackage.getLoadStatus() == LoadStatus.LOADED && !mobileScenePackage.getScenes()
-          .isEmpty()) {
-        // get a reference to the first scene in the mobile scene package, which is of a section of philadelphia
-        ArcGISScene philadelphiaScene = mobileScenePackage.getScenes().get(0);
-        // add the scene to the AR view's scene view
-        mArView.getSceneView().setScene(philadelphiaScene);
-        // set the base surface to fully opaque
-        philadelphiaScene.getBaseSurface().setOpacity(0);
-        // let the camera move below ground
-        philadelphiaScene.getBaseSurface().setNavigationConstraint(NavigationConstraint.NONE);
-        mHasConfiguredScene = true;
-        // set translation factor and origin camera for scene placement in AR
-        updateTranslationFactorAndOriginCamera(philadelphiaScene, plane);
-      } else {
-        String error = "Failed to load mobile scene package: " + mobileScenePackage.getLoadError()
-            .getMessage();
-        Toast.makeText(this, error, Toast.LENGTH_LONG).show();
-        Log.e(TAG, error);
-      }
-    });
-  }
-
-  /**
-   * Load the scene's first layer and calculate its geographical width. Use the scene's width and ArCore's assessment
-   * of the plane's width to set the AR view's translation transformation factor. Use the center of the scene, corrected
-   * for elevation, as the origin camera's look at point.
-   *
-   * @param scene to display
-   * @param plane detected by ArCore to which the scene should be pinned
-   */
-  private void updateTranslationFactorAndOriginCamera(ArcGISScene scene, Plane plane) {
-    // load the scene's first layer
-    scene.getOperationalLayers().get(0).loadAsync();
-    scene.getOperationalLayers().get(0).addDoneLoadingListener(() -> {
-      // get the scene extent
-      Envelope layerExtent = scene.getOperationalLayers().get(0).getFullExtent();
-      // calculate the width of the layer content in meters
-      double width = GeometryEngine
-          .lengthGeodetic(layerExtent, new LinearUnit(LinearUnitId.METERS), GeodeticCurveType.GEODESIC);
-      // set the translation factor based on scene content width and desired physical size
-      mArView.setTranslationFactor(width / plane.getExtentX());
-      // find the center point of the scene content
-      Point centerPoint = layerExtent.getCenter();
-      // find the altitude of the surface at the center
-      ListenableFuture<Double> elevationFuture = mArView.getSceneView().getScene().getBaseSurface()
-          .getElevationAsync(centerPoint);
-      elevationFuture.addDoneListener(() -> {
-        try {
-          double elevation = elevationFuture.get();
-          // create a new origin camera looking at the bottom center of the scene
-          mArView.setOriginCamera(
-              new Camera(new Point(centerPoint.getX(), centerPoint.getY(), elevation), 0, 90, 0));
-        } catch (Exception e) {
-          Log.e(TAG, "Error getting elevation at point: " + e.getMessage());
-        }
-      });
     });
   }
 
@@ -191,7 +125,7 @@ public class MainActivity extends AppCompatActivity {
    */
   private void requestPermissions() {
     // define permission to request
-    String[] reqPermission = { Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.CAMERA };
+    String[] reqPermission = { Manifest.permission.CAMERA };
     int requestCode = 2;
     if (ContextCompat.checkSelfPermission(this, reqPermission[0]) == PackageManager.PERMISSION_GRANTED) {
       setupArView();
